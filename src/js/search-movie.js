b482300a8122ee9c0c217331878d9f4724f39e06
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-12-19 23:35:15 +0200

    Create search-movie.js

---
+++ src/js/search-movie.js
@@ -0,0 +1,41 @@
+import { getRefs } from './get-refs';
+import { FilmsApiService } from './films-service';
+import { filmTpl } from './films-gallery';
+import { combineGenres } from './get-genres';
+
+const refs = getRefs();
+
+const filmsApiService = new FilmsApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+async function onSearch(e) {
+  e.preventDefault();
+  const form = e.target;
+  filmsApiService.query = e.target.elements.search.value.trim();
+
+  if (filmsApiService.query === '') {
+    onError();
+    return;
+  }
+  const genres = await combineGenres();
+  const filmOnSearch = await filmsApiService.fetchFilmsOnSearch(
+    filmsApiService.query
+  );
+  clearGalleryContainer();
+  renderFilmGallery(filmOnSearch, genres);
+  form.reset();
+}
+
+function onError() {
+  alert('Please type something');
+  console.log('ERROR');
+}
+
+function renderFilmGallery(films, genres) {
+  refs.filmGallery.insertAdjacentHTML('beforeend', filmTpl(films, genres));
+}
+
+function clearGalleryContainer() {
+  refs.filmGallery.innerHTML = '';
+}
