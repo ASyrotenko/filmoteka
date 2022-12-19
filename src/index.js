94768f2b80207c8aca324ce51733f290f6f53b1d
ASyrotenko <andreysyrotenko@gmail.com>
2022-12-19 22:02:17 +0200

    index

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './js/modal-team';
+import './js/addvideo';
+import './js/firebase-auth';
+import './js/firebase-db-index';
+import './js/firebase-class';
+import './js/movie-card';
+import './js/search-movie';
+// import './js/pagination';
+
+import { FilmsApiService } from './js/films-service';
+import { filmTpl } from './js/films-gallery';
+import { getRefs } from './js/get-refs';
+import { combineGenres } from './js/get-genres';
+// import { pagination } from './js/pagination';
+const refs = getRefs();
+
+function renderFilmGallery(films, genres) {
+  refs.filmGallery.insertAdjacentHTML('beforeend', filmTpl(films, genres));
+  //remove spinner
+  document.querySelector('.spinner').style.display = 'none';
+}
+
+export const filmsApiService = new FilmsApiService();
+async function load() {
+  const genres = await combineGenres();
+  const filmsTrending = await filmsApiService.fetchFilmsTrending();
+  renderFilmGallery(filmsTrending, genres);
+}
+load();
