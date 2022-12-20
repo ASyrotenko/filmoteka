cae4afb08f285d3e0446deab4000c7ffb201e354
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2022-12-20 08:19:05 +0200

    start

---
+++ src/js/firebase-auth.js
@@ -0,0 +1,21 @@
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { Firebase, auth } from './firebase-class';
+
+const singInBtn = document.querySelector('#signin');
+const singOutBtn = document.querySelector('#signout');
+const userPhoto = document.querySelector('#googleUser');
+
+const firebase = new Firebase();
+
+onAuthStateChanged(auth, user => {
+  if (user) {
+    singInBtn.classList.add('signOut');
+    singOutBtn.classList.add('signIn');
+    userPhoto.innerHTML = `<img class="user-img" src="${user.photoURL}">`;
+    userPhoto.style.display = 'block';
+  }
+});
+
+singInBtn.addEventListener('click', firebase.singInWithGoogle);
+singOutBtn.addEventListener('click', firebase.singOutUser);
