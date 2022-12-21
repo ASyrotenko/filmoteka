a9c7de5a3af28613e697c4aac0c5845c1ebeabf9
ASyrotenko <andreysyrotenko@gmail.com>
2022-12-21 13:59:55 +0200

    all-done

--- src/my-library.js
+++ src/my-library.js
@@ -1,6 +1,5 @@
 import './js/firebase-auth';
 import './js/firebase-class';
 import './js/firebase-db-library';
-import './js/firebase-db-index';
 import './js/change-color-scheme.js';
 import './js/modal-team';
