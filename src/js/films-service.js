43fbd1f8530350c6a702046d8ffbeff5d1ed1be4
YevheniiaVier <gotashk1983@gmail.com>
2022-12-23 18:26:12 +0200

    corrects img

--- src/js/films-service.js
+++ src/js/films-service.js
@@ -37,13 +37,11 @@ export class FilmsApiService {
         .setAttribute('style', 'display:inline !important');
       const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${this.searchQuery}&page=${this.page}&include_adult=false`;
       const response = await axios.get(url);
-      //   this.incrementPage();
-      // Notiflix.Notify.failure('CHECK!!!!!!!!fetchFilmsOnSearch');
-      console.log('CHECK!!!!!!!!fetchFilmsOnSearch');
+
       return response.data;
     } catch (error) {
-      alert(
-        'Sorry, there are no films matching your search query. Please try again.'
+      Notiflix.Notify.failure(
+        'Sorry, there are no films matching your search query. Please try again'
       );
     }
   }
@@ -55,7 +53,6 @@ export class FilmsApiService {
       return response.data;
     } catch (error) {
       return Notiflix.Notify.failure('Error - Mov is is not available');
-      // console.log('ERROR - Mov is is not available');
     }
   }
   async fetchMovieVideo(movieId) {
