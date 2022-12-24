36ecea149dbd0d9874bb14f3711b4b343e158d6d
MakiievOleksandr <alsermak@gmail.com>
2022-12-24 22:27:33 +0200

    add throttle

--- src/js/sticky-header.js
+++ src/js/sticky-header.js
@@ -1,24 +1,24 @@
 import { debounce } from 'debounce';
-
+import throttle from 'lodash.throttle';
 const headerEl = document.querySelector('.header');
-const filmSectionEl = document.querySelector('.film__section');
+const filmSectionEl = document.querySelector('.container__slider');
 
 let lastScrollTop = 0;
 
-window.addEventListener('scroll', debounce(stickyHeaderOnScroll, 200), false);
+window.addEventListener('scroll', throttle(stickyHeaderOnScroll, 500), false);
 
 function stickyHeaderOnScroll() {
   let scrollPos = window.scrollY || document.documentElement.scrollTop;
   if (scrollPos > lastScrollTop) {
     // downscroll code
-    // headerEl.classList.remove('appearing__header-js');
-    // headerEl.classList.add('disappearing__header-js');
+    headerEl.classList.remove('appearing__header-js');
+    headerEl.classList.add('disappearing__header-js');
   } else {
     // upscroll code
-    // headerEl.classList.remove('disappearing__header-js');
-    // headerEl.classList.add('sticky__header');
+    headerEl.classList.remove('disappearing__header-js');
+    headerEl.classList.add('sticky__header');
     headerEl.classList.add('appearing__header-js');
-    // filmSectionEl.classList.add('film__section-js');
+    filmSectionEl.classList.add('container__slider-js');
   }
 
   lastScrollTop = scrollPos <= 0 ? 0 : scrollPos; // For Mobile or negative scrolling
